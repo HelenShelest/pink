dae778a72ff326c5e68b9e3803c049509a3a5647
HelenShelest <asto.shelest@yandex.ru>
2016-08-10 15:54:54 +0300

    Начала работу над отображением меню

--- js/common.js
+++ js/common.js
@@ -1,1 +1,9 @@
-﻿
+//Добавление классов для подключения стилей отображения мобильного меню.
+
+window.onload = function () {
+  for (i = 0; i < 3; i++) {
+  document.getElementsByClassName('main-menu__toggle')[i].classList.add('main-menu__toggle-icon');
+  }
+  document.getElementById('nav').classList.add('main-menu__list-hidden');
+};
+
