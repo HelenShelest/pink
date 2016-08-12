0d6b23209efc8ca6ee63d6ca525749b035778822
HelenShelest <asto.shelest@yandex.ru>
2016-08-12 10:20:59 +0300

    Доработала меню для мобильных

--- js/common.js
+++ js/common.js
@@ -1,9 +1,42 @@
 //Добавление классов для подключения стилей отображения мобильного меню.
 
 window.onload = function () {
-  for (i = 0; i < 3; i++) {
-  document.getElementsByClassName('main-menu__toggle')[i].classList.add('main-menu__toggle-icon');
-  }
-  document.getElementById('nav').classList.add('main-menu__list-hidden');
+  document.getElementById('btn').classList.add('page-header__btn');
+  document.getElementById('nav').classList.add('main-menu--hidden');
 };
 
+$(document).ready(function () {
+	$('#btn').click(function () {
+    $('#btn').toggleClass('page-header__btn--active')
+		$('#nav').toggleClass('main-menu--hidden');
+    $('.page-header').toggleClass('page-header--active');
+		});
+	});
+
+$(document).ready(function () {
+	$('.info-link').click(function () {
+		$('#nav').toggleClass('main-menu--hidden');
+    $('.page-header').toggleClass('page-header--active');
+		});
+	});
+
+$(document).ready(function () {
+	$('.feature-link').click(function () {
+		$('#nav').toggleClass('main-menu--hidden');
+    $('.page-header').toggleClass('page-header--active');
+		});
+	});
+
+$(document).ready(function () {
+	$('.review-link').click(function () {
+		$('#nav').toggleClass('main-menu--hidden');
+    $('.page-header').toggleClass('page-header--active');
+		});
+	});
+
+$(document).ready(function () {
+	$('.price-link').click(function () {
+		$('#nav').toggleClass('main-menu--hidden');
+    $('.page-header').toggleClass('page-header--active');
+		});
+	});
